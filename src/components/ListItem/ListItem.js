99f3969563523bd3f4010efb370cb827b66adc29
Peter CHEN <yongdechen@gmail.com>
2021-07-09 13:14:48 +1000

    Finished the Unit Test

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -5,25 +5,30 @@ export default function ListItem({todo, deleteTodo, updateTodos}) {
     const {title, completed, id} = todo;
     return (
         <div className='list-item'>           
-           <div className='item-title'>
+            <div className='item-title'>
                 <input 
-                type='checkbox' 
-                checked={completed}
-                data-testid={`checkbox-${id}`}
-                name={`checkbox-${id}`} 
-                onChange={()=> updateTodos(id)}
+                    type='checkbox' 
+                    checked={completed}
+                    data-testid={`checkbox-${id}`}
+                    name={`checkbox-${id}`} 
+                    onChange={()=> updateTodos(id)}
                 />
                 <label 
-                htmlFor={`checkbox-${id}`}
-                onClick={()=> updateTodos(id)}
-                className={`${completed? styles.completed:''}`}
-                 >{title}
+                    htmlFor={`checkbox-${id}`}
+                    data-testid={`labelOfCheckbox-${id}`}
+                    onClick={()=> updateTodos(id)}
+                    className={`${completed? styles.completed:''}`}
+                    >
+                        {title}
                 </label> 
-           </div>
-           <button 
-           className={styles.closeBtn}
-           data-testid={`closeBtn-${id}`}
-           onClick = {()=>deleteTodo(id)}>X</button>           
+            </div>
+            <button 
+                className={styles.closeBtn}
+                data-testid={`closeBtn-${id}`}
+                onClick = {()=>deleteTodo(id)}
+            >
+                X
+            </button>
         </div>
     )
 }
